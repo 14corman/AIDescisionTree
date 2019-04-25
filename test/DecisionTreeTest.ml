e033d5f861c59ba3522942cc70a36f83194a305f
coryedwards <cory-edwards@uiowa.edu>
2019-04-25 07:52:41 -0500

    Finalized the code
    Added comments to everything
    Fixed the last few bugs in creating the tree.
    Updated the testProgram.exe to be the new code.

--- test/DecisionTreeTest.ml
+++ test/DecisionTreeTest.ml
@@ -1,15 +1,29 @@
-open DecisionTreev2;;
+open DecisionTreev2
 
+(* x is a map with the key being the feautre name and the value being a list of examples for that feauture. The original data would look like:
+    h  w  a  p  e  r  ...
+    0  1  0  1  1  1
+    0  1  0  1  1  1
+    0  1  1  0  1  1
+    0  1  1  0  0  1
+    1  1  1  0  1  1
+    0  1  1  0  1  1
+    ...
+
+    The letters are the abbreviations of the feature names.*)
 let x = DTree.Feature_map.empty
 let x = DTree.Feature_map.add "handicaped-infants"          [0; 0; 0; 0; 1; 0; 0; 0; 0] x
 let x = DTree.Feature_map.add "water-project-cost"          [1; 1; 1; 1; 1; 1; 1; 1; 1] x
 let x = DTree.Feature_map.add "adoption-of-the-budget"      [0; 0; 1; 1; 1; 1; 0; 0; 0] x
 let x = DTree.Feature_map.add "physician-fee-freeze"        [1; 1; 0; 0; 0; 0; 1; 1; 1] x
 let x = DTree.Feature_map.add "el-salvador-aid"             [1; 1; 1; 0; 1; 1; 1; 1; 1] x
-let x = DTree.Feature_map.add "religious-groups-in-school"  [1; 1; 1; 1; 1; 1; 1; 1; 1] x;;
+let x = DTree.Feature_map.add "religious-groups-in-school"  [1; 1; 1; 1; 1; 1; 1; 1; 1] x
 
-let y = [1; 1; 0; 0; 0; 0; 0; 1; 1];;
+(* list of examples in the same order as the lists of examples are given in x. *)
+let y = [1; 1; 0; 0; 0; 0; 0; 1; 1]
 
+(* Map with key being feature name and value being the number of attributes for this feature.
+   All features here are boolean featurues so they only have 0 or 1 which is why they all have 1. *)
 let attrs = DTree.Feature_map.empty
 let attrs = DTree.Feature_map.add "handicaped-infants" 1 attrs
 let attrs = DTree.Feature_map.add "water-project-cost" 1 attrs
