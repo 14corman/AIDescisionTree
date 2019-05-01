d5a3eb84cbaf2f6af29297aae7a305c56f23c868
coryedwards <cory-edwards@uiowa.edu>
2019-05-01 08:38:31 -0500

    fixing data in main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
 open TreeClassifier;;
 open Parser;;
 
-let (feature_map, labels, value_map) = parse "C:\\OCaml64\\home\\CoryEdwards\\AIDescisionTree\\datahouse-votes-84.csv" ;;(*Sys.argv.(1) *);;
+let (feature_map, labels, value_map) = parse "data/house-votes-84.csv" ;;(*Sys.argv.(1) *);;
 let g = build_tree feature_map labels value_map;;
 let () = print_graph g;;
